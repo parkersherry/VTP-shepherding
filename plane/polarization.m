c30e58a381777ff403a02eca5a24f5ff41e58375
Parker S. <157440166+parkersherry@users.noreply.github.com>
2025-06-23 11:29:17 -0400

    Delete plane directory

--- plane/polarization.m
+++
@@ -1,6 +0,0 @@
-function pol = polarization(U,Ndogs)
-
-normedDisplacement = U./vecnorm(U,2,2);
-Nsheep = numel(U)/2 - Ndogs;
-pol = vecnorm(sum(normedDisplacement(Ndogs+1:end,:)),2,2)/Nsheep;
-
--- plane/untitled.m
+++
@@ -1,4 +0,0 @@
-a = Target();
-b = [0 0];
-h = homeToTarget(a,b);
-disp(h)

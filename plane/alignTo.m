946e709e3d71ac86448f13f92f0c6950e960e88f
Mikey Baker <123831526+mbaker20@users.noreply.github.com>
2025-06-23 10:57:54 -0400

    Added Plane Files

---
+++ plane/alignTo.m
@@ -0,0 +1,59 @@
+function a = alignTo(X,U,nbhd,transition_func, sheepThetaVision,Ndogs,vMaxSheep)
+
+arguments
+    X (:,2) double
+    U (:,2) double
+    nbhd (:,1) cell {mustBeEqualSize(nbhd,U)}
+    transition_func (1,:) char
+    sheepThetaVision
+    Ndogs
+    vMaxSheep
+end
+N = size(U,1);
+velBaseds = ones(N,1);
+a = zeros(N,2);
+g = @(x) transition(x,transition_func);
+w = @(x) g(1/pi*acos(x));
+for i=Ndogs+1:N
+    ui = U(i,:);
+    ui = ui/norm(ui,2);
+    ui(isnan(ui)) = 0;
+    J = nbhd{i};
+    velBaseds(i) = 2.-g(vecnorm(max(U(J,:)))/vMaxSheep);
+    for j=1:size(J,2)
+        s_x = X(i,:) - X(nbhd{i}(j),:);
+        s_x = s_x./vecnorm(s_x,2,2);
+        if acos(dot(s_x, ui)) > sheepThetaVision(i)/2
+            continue
+        end
+        uj = U(J(j),:);
+        uj = uj/norm(uj,2);
+        uj(isnan(uj)) = 0;
+        s = dot(ui,uj);
+        % due to numerical error, it can happen that |s|>1, if this is not
+        % corrected, w(s) (which calls acos) will attempt to input an
+        % imaginary value to g. The complex-valued acos function is
+        % continuous at ±1 so this correction is well-behaved.
+        if abs(s) > 1
+            s = sign(s);
+        end
+        a(i,:) = a(i,:) + w(s)*uj;
+    end
+end
+a = (1/6)*a;
+
+s = ones(N,1);
+
+s(Ndogs+1:end,:) = 2-arrayfun(g,abs(vecnorm(U(Ndogs+1:end,:),2,2)./vMaxSheep));
+a = s.*a;
+a = velBaseds.*a;
+end
+
+% validation
+function mustBeEqualSize(a,b)
+if size(a,1) ~= size(b,1)
+    eid = 'Size:equalSize';
+    msg = 'second input must have same length as first';
+    throwAsCaller(MException(eid,msg));
+end
+end
---
+++ plane/gradPreferenceField.m
@@ -0,0 +1,70 @@
+function grad = gradPreferenceField(X,c,scalarField)
+%-----------------%
+%inputs:
+% -> X = the positions at which to find the gradient
+% -> c = an array of coefficients for the Fourier components (to be
+%        randomly generated
+% -> scalarField = a string corresponding to the scalar field we want the
+%                  gradient of
+
+%outputs:
+% -> grad = an array of size X, indicating the direction each element of X
+%           should go in to follow the grad of the scalar field
+%-----------------%
+grad = zeros(size(X));
+switch scalarField
+    case '2gaussiansPositive'
+        offset = 20;
+        gaussian1 = -1.*exp(-1.*((X(:,1)+offset).^2 + (X(:,2)+offset).^2)./100);
+        gaussian1 = cat(2,gaussian1,gaussian1);
+        
+        gaussian2 = exp(-1.*((X(:,1)-offset).^2 + (X(:,2)-offset).^2)./100);
+        gaussian2 = cat(2,gaussian2,gaussian2);
+        
+        grad =  gaussian1.*(offset+X)+gaussian2.*(offset-X);
+    case '1gaussian'
+        offset = 0;
+        gaussian1 = -1.*exp(-1.*((X(:,1)+offset).^2 + (X(:,2)+offset).^2)./100);
+        gaussian1 = cat(2,gaussian1,gaussian1);
+        grad =  gaussian1.*(offset+X);
+    case 'quadraticTrap'
+        grad = -2.*X;
+    case '2gaussiansOppSign'
+        offset = 20;
+        gaussian1 = exp(-1.*((X(:,1)+offset).^2 + (X(:,2)+offset).^2)./100);
+        gaussian1 = cat(2,gaussian1,gaussian1);
+        
+        gaussian2 = exp(-1.*((X(:,1)-offset).^2 + (X(:,2)-offset).^2)./100);
+        gaussian2 = cat(2,gaussian2,gaussian2);
+        grad = gaussian1.*(offset+X)+gaussian2.*(offset-X);
+    case 'manyPeaks'
+        exponential = exp(-1.*(X(:,2).^2)./4);
+        grad = [8.*exponential.*cos(2.*X(:,1)), -2.*X(:,2).*exponential.*sin(2.*X(:,1))];
+        grad = grad./10;
+    case 'fence'
+        a = 0.44;
+        inside = (X(:,1)<20).*(X(:,2)<20);
+        indices = find(inside);
+        grad(indices,:) = (-400/a^2).*[exp((-1/a).*(X(indices,1)-20).^2) exp((-1/a).*(X(indices,2)-20).^2)];
+        indices = find(~inside);
+        grad(indices,:) = (400/a^2).*[exp((-1/a).*(X(indices,1)-20).^2) exp((-1/a).*(X(indices,2)-20).^2)];
+        indicesToZero = (X(:,1)<25).*(X(:,1)>15).*(X(:,2)<25).*(X(:,2)>15);
+        grad(find(indicesToZero),:) = 0;
+    case 'zero'
+        return;
+end
+pert = zeros(numel(X)/2,2);
+if ~(strcmp('fence',scalarField))
+    for n = 1:numel(c(:,1,1))
+        for m = 1:numel(c(1,:,1))
+            pert = pert + (1/(n*n*m*m)).*c(n,m,1).*[n.*cos(n.*X(:,1)).*sin(m.*X(:,2)), m.*cos(n.*X(:,1)).*sin(m.*X(:,2))];
+            pert = pert + (1/(n*n*m*m)).*c(n,m,2).*[n.*cos(n.*X(:,1)).*cos(m.*X(:,2)), -1.*m.*sin(n.*X(:,1)).*sin(m.*X(:,2))];
+            pert = pert + (1/(n*n*m*m)).*c(n,m,3).*[-1.*n.*sin(n.*X(:,1)).*cos(m.*X(:,2)), -1.*m.*cos(n.*X(:,1)).*sin(m.*X(:,2))];
+        end
+    end
+    pert = pert./(numel(c(:,1,1)));
+end
+%disp(grad)
+%quiver(X(:,1),X(:,2),pert(:,1),pert(:,2))
+grad = grad + pert;
+
